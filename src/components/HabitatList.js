86a5b2e66590577e38a753f33a22959a48a235d9
ericcorona1 <ericcorona3@gmail.com>
2023-08-22 13:16:24 -0700

    added biomes with habitats data

--- src/components/HabitatList.js
+++ src/components/HabitatList.js
@@ -1,37 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { graphql, useStaticQuery } from "gatsby";
 
 const query = graphql`
-  query {
-    allPokemonHabitatJson {
-      nodes {
-        biome_name
-        habitat_name
-        pokemon_name
-      }
+query {
+  allBiomesJson {
+    nodes {
+      biome_description
+      biome_name
+      habitat_name
     }
   }
-`;
+}
+`
 
-const HabitatList = ({habitats, biomes}) => {
+const HabitatList = () => {
   const {
-    allPokemonHabitatJson: { nodes: habitats },
+    allBiomesJson: { nodes: biomes },
   } = useStaticQuery(query);
+  const [selectedBiome, setSelectedBiome] = useState('');
+  const habitatsInBiome = biomes[selectedBiome] || [];
   return (
     <div>
       <p>Habitats List</p>
       <div className="habitats-list">
-        {biomes ? habitats.map((habitatEntry) => {
+        {/* {biomes ? habitats.map((habitatEntry) => {
             const habitatName = habitatEntry.habitat_name;
             return (
                 <p>{habitatName}</p>
-                 <div className="buttonContainer">
-                    <button>+</button>
-                    <span className="counterOutput"></span>
-                    <button>-</button>
-                </div>
+                //  <div className="buttonContainer">
+                //     <button>+</button>
+                //     <span className="counterOutput"></span>
+                //     <button>-</button>
+                // </div>
             )
-            }) : ""}
+            }) : ""} */}
       </div>
     </div>
   );
