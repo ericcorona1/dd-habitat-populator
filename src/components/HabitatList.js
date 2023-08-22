9191c52b852412777f5263f7c192b17a4f019c03
ericcorona1 <ericcorona3@gmail.com>
2023-08-22 09:32:44 -0700

    habitat list
    adding logic to display habitats of selected biomes

--- src/components/HabitatList.js
+++ src/components/HabitatList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { graphql } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
 
 const query = graphql`
   query {
@@ -13,9 +13,28 @@ const query = graphql`
   }
 `;
 
-const HabitatList = () => {
-    
-  return <div>HabitatList</div>;
+const HabitatList = ({habitats, biomes}) => {
+  const {
+    allPokemonHabitatJson: { nodes: habitats },
+  } = useStaticQuery(query);
+  return (
+    <div>
+      <p>Habitats List</p>
+      <div className="habitats-list">
+        {biomes ? habitats.map((habitatEntry) => {
+            const habitatName = habitatEntry.habitat_name;
+            return (
+                <p>{habitatName}</p>
+                 <div className="buttonContainer">
+                    <button>+</button>
+                    <span className="counterOutput"></span>
+                    <button>-</button>
+                </div>
+            )
+            }) : ""}
+      </div>
+    </div>
+  );
 };
 
 export default HabitatList;
