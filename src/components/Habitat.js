4f40bdfd0b02da2bc5672919415e169dce41db15
ericcorona1 <ericcorona3@gmail.com>
2023-08-22 22:29:01 -0700

    got rid of consts in habitat that were lifted

--- src/components/Habitat.js
+++ src/components/Habitat.js
@@ -1,25 +1,7 @@
 import React from "react";
 import HabitatList from "./HabitatList";
 
-const query = graphql`
-  query {
-    allBiomesJson {
-      nodes {
-        biome_description
-        biome_name
-        habitat_name
-      }
-    }
-  }
-`;
-
 const Habitat = ({ habitats }) => {
-  const {
-    allBiomesJson: { nodes: biomes },
-  } = useStaticQuery(query);
-  const [selectedBiome, setSelectedBiome] = useState("");
-  const [allSelectedBiomes, setAllSelectedBiomes] = useState([]);
-  const habitatsInBiome = biomes[selectedBiome] || [];
   return (
     <section>
       <h2>SELECT HABITAT</h2>
