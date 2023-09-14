72b17a98c2897c6cf93d24445a5e5a0b437664f1
ericcorona1 <ericcorona3@gmail.com>
2023-09-14 01:50:49 -0700

    fetch all pokemon

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,23 +9,30 @@ exports.sourceNodes = async ({
 }) => {
   const { createNode } = actions;
 
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=1010"); // Retrieves data for all 1010 Pokémon
   const json = await response.json();
   const { results = [] } = json;
 
-  const pokemon = await Promise.all(
+  // Define a function to fetch data for an individual Pokémon by URL
+  const fetchPokemonData = async (url) => {
+    const pokeResponse = await fetch(url);
+    const pokemonData = await pokeResponse.json();
+    return {
+      ...pokemonData,
+      externalId: pokemonData.id, // Preserve the original ID
+    };
+  };
+
+  // Iterate through the list of Pokémon and fetch data for each one
+  const pokemonDataArray = await Promise.all(
     results.map(async (result) => {
       const { url } = result;
-      const pokeResponse = await fetch(url);
-      const pokemonData = await pokeResponse.json();
-      return {
-        ...pokemonData,
-        externalId: pokemonData.id, // Preserve the original ID
-      };
+      return fetchPokemonData(url);
     })
   );
 
-  pokemon.forEach((node, index) => {
+  // Create Gatsby nodes for each Pokémon
+  pokemonDataArray.forEach((node, index) => {
     createNode({
       ...node,
       id: createNodeId(`${NODE_TYPE}-${node.id}`), // Generate a unique Gatsby node ID
--- src/components/OverlayHabitat.js
+++ src/components/OverlayHabitat.js
@@ -1,32 +1,52 @@
 import { graphql, useStaticQuery } from "gatsby";
 import React from "react";
 
-// 
+const query = graphql`
+query {
+  allPokemon {
+    nodes {
+      externalId
+      name
+      sprites {
+        front_default
+        other {
+          official_artwork {
+            front_default
+          }
+        }
+      }
+      types {
+        type {
+          name
+        }
+        slot
+      }
+    }
+    totalCount
+  }
+}
+`;
 
 const OverlayHabitat = ({ habitatName, pokemons, count }) => {
-  // const {
-  //   allPokemon: { nodes: pokemonData },
-  // } = useStaticQuery(query);
-  // const findPokemonData = (pokemonId) => {
-  //   return pokemonData.find((pokemonCheck) => {
-  //     const adjustedId = pokemonCheck.id - 1; // Reduce pokemonCheck.id by one
-  //     console.log(pokemonCheck);
-  //     return adjustedId === pokemonId;
-  //   });
-  // };
+  const {
+    allPokemon: { nodes: pokemonData },
+  } = useStaticQuery(query);
+  console.log(pokemonData);
+  const findPokemonData = (pokemonId) => {
+    return pokemonData.find((pokemonCheck) => pokemonCheck.externalId === pokemonId);
+  };
   
   const listedPokemon = pokemons.slice(0, count);
-  console.log(listedPokemon);
 
   return (
     <div>
       <h3>{habitatName}</h3>
       <ul>
         {listedPokemon.map((listedPokemon) => {
-          // const foundPokemon = findPokemonData(listedPokemon.pokemon_id);
+          const foundPokemon = findPokemonData(listedPokemon.pokemon_id);
           return (
             <li key={listedPokemon.pokemon_id}>
-              {/* {listedPokemon.pokemon_id}:{foundPokemon && foundPokemon.name} */}
+              {listedPokemon.pokemon_id}:{foundPokemon && foundPokemon.name}
             </li>
           );
         })}
