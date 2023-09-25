3c26efa05b27493c730213927d9a9ace1a99e2c4
eric <ericcorona3@gmail.com>
2023-09-24 21:36:50 -0700

    class habitatSection

--- src/components/Habitat.js
+++ src/components/Habitat.js
@@ -1,25 +1,30 @@
 import React from "react";
 import HabitatList from "./HabitatList";
 
-const Habitat = ({ selectedBiome, increment, decrement, handleNumberChange, onEmptyInput }) => {
+const Habitat = ({
+  selectedBiome,
+  increment,
+  decrement,
+  handleNumberChange,
+  onEmptyInput,
+}) => {
   const { habitats } = selectedBiome;
   return (
-    <section>
+    <section className="habitatSection">
       <h2>SELECT HABITAT</h2>
       {Object.keys(habitats).map((habitatId) => {
-          return (
-            <HabitatList
-              biomeId={selectedBiome.biomeId}
-              habitatId={habitatId} // Make sure to include a unique key when rendering in a loop
-              increment={increment}
-              decrement={decrement}
-              habitat={habitats}
-              handleNumberChange={handleNumberChange}
-              onEmptyInput={onEmptyInput}
-            />
-          );
-        })
-      }
+        return (
+          <HabitatList
+            biomeId={selectedBiome.biomeId}
+            habitatId={habitatId} // Make sure to include a unique key when rendering in a loop
+            increment={increment}
+            decrement={decrement}
+            habitat={habitats}
+            handleNumberChange={handleNumberChange}
+            onEmptyInput={onEmptyInput}
+          />
+        );
+      })}
     </section>
   );
 };
